5389e2f75fbbc01eab69cd2998cb393bfaf22788
shreya suman <shreyasumanck@gmail.com>
2019-05-31 16:33:55 -0400

    Initial commit

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burgers_db;
+use burgers_db;
+
+CREATE TABLE burgers_tbl(
+    id INT PRIMARY KEY auto_increment,
+    burger_name VARCHAR(30) NOT NULL,
+    devoured_burgers BOOLEAN,
+    burger_time TIMESTAMP,
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers_tbl(burger_name) VALUES ('Hamburger');
+INSERT INTO burgers_tbl(burger_name) VALUES ('Chicken Sandwich');
+INSERT INTO burgers_tbl(burger_name) VALUES ('vegan burger');
+INSERT INTO burgers_tbl(burger_name) VALUES ('Sea Food burger');
