c3b603afcaed940579f5ad342103d8b42e40af49
shreya suman <shreyasumanck@gmail.com>
2019-07-30 19:00:31 -0400

    changed file paths for heroku deployment

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,40 +9,41 @@ burger_router.use(bodyParser.text());
 burger_router.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 burger_router.get('/', function(req,res){
-  res.redirect('/burger');
+  res.redirect(path.join(__mkdir, '/burger'));
 });
 
-burger_router.get('/burger', function(req,res){
+burger_router.get(path.join(__mkdir, '/burger'), function(req,res){
   burger_call.read(function(data){
    
     var hbs_object = {burger: data};
    
-    res.render('index', hbs_object);
+    res.render(path.join(__dirname, 'index'), hbs_object);
   });
 });
 
-burger_router.post('/burger/add', function(req, res){
+burger_router.post(path.join(__dirname, '/burger/add'), function(req, res){
 
   burger.create(req.body.burger_name, function(req,res) {
 
     console.log(result);
   
-  burger_call.insert(req.body.user_burger, function(data){
-    res.redirect('/burger');
-  });
+    burger_call.insert(req.body.user_burger, function(data){
+      res.redirect(path.join(__mkdir, '/burger'));
+    });
+  })
 });
 
-burger_router.put('/burger/update/:id?', function(req,res){
+burger_router.put(path.join(__dirname, '/burger/update/:id?'), function(req,res){
   var user_id = parseInt(req.params.id);
   burger_call.update(user_id, function(data){
-    res.redirect('/burger');
+    res.redirect(path.join(__mkdir, '/burger'));
   });
 });
 
 burger_router.put('/burger/delete/:id?', function(req,res){
   var user_id = parseInt(req.params.id);
   burger_call.delete(user_id, function(data){
-    res.redirect('/burger');
+    res.redirect(path.join(__mkdir, '/burger'));
   });
 });
 module.exports = burger_router;
